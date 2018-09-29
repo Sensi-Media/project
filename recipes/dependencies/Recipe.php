fbad919c56e63965a2e25e920b70a12318a29f94
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-29 15:28:04 +0200

    do something with the passed repositories

--- recipes/dependencies/Recipe.php
+++ recipes/dependencies/Recipe.php
@@ -1,12 +1,20 @@
 <?php
 
 use Codger\Generate\Recipe;
+use Codger\Generate\Language;
 
-return function (string ...$dependencies) : Recipe {
+return function (string ...$repositories) : Recipe {
     $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
         protected $template = 'dependencies.html.twig';
     };
     $recipe->output('src/dependencies.php');
+    array_walk($repositories, function (&$repository) {
+        $repository = [
+            'variable' => Language::convert($repository, Language::TYPE_VARIABLE),
+            'namespace' => Language::convert($repository, Language::TYPE_NAMESPACE),
+        ];
+    });
+    $recipe->set('repositories', $repositories);
     return $recipe;
 };
 
