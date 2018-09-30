ade79a9fc2ff025d9d197912318ef2a8625c73a4
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-30 15:54:45 +0200

    set vendor

--- recipes/dependencies/Recipe.php
+++ recipes/dependencies/Recipe.php
@@ -3,11 +3,15 @@
 use Codger\Generate\Recipe;
 use Codger\Generate\Language;
 
-return function (string ...$repositories) : Recipe {
+return function (string $vendor, string ...$repositories) : Recipe {
     $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
         protected $template = 'dependencies.html.twig';
     };
     $recipe->output('src/dependencies.php');
+    switch ($vendor) {
+        case 'mysql': $recipe->set('vendor', 'Mysql'); break;
+        case 'pgsql': $recipe->set('vendor', 'Postgresql'); break;
+    }
     array_walk($repositories, function (&$repository) {
         $repository = [
             'variable' => Language::convert($repository, Language::TYPE_VARIABLE),
