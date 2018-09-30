d88b1a4161793351e2c5646d7a230a024c7cedea
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-30 14:56:12 +0200

    config recipe

---
+++ recipes/config/Recipe.php
@@ -0,0 +1,12 @@
+<?php
+
+use Codger\Generate\Recipe;
+
+return function (string $project) : Recipe {
+    $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
+        protected $template = 'config.html.twig';
+    };
+    $recipe->out('ServerConfig.json');
+    return $recipe;
+};
+
