fa63c3aa13f883d543db7fdcc3d5c5f342cc03c4
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-30 14:55:29 +0200

    set variables

--- recipes/environment/Recipe.php
+++ recipes/environment/Recipe.php
@@ -2,11 +2,17 @@
 
 use Codger\Generate\Recipe;
 
-return function () : Recipe {
+return function (string $project, string $database, string $user, string $pass) : Recipe {
     $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
         protected $template = 'environment.html.twig';
     };
     $recipe->output('Envy.json');
+    $recipe->set('project', $project);
+    $recipe->set('database', $database);
+    $recipe->set('user', $user);
+    $recipe->set('pass', $pass);
+    // This can easily be changed.
+    $recipe->set('host', "$project.nl");
     return $recipe;
 };
 
