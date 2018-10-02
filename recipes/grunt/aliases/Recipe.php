c33fce7926fb1694460d18b41e95b62167ba6eef
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-02 11:45:34 +0200

    recipe for grunt aliases

---
+++ recipes/grunt/aliases/Recipe.php
@@ -0,0 +1,12 @@
+<?php
+
+use Codger\Generate\Recipe;
+
+return function () : Repice {
+    $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
+        protected $template = 'grunt/aliases.html.twig';
+    };
+    $recipe->output('grunt/aliases.js');
+    return $recipe;
+};
+
