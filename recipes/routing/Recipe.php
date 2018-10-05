4f10802e35a3841cfa4c3c9256c1509cf53642a0
Marijn Ophorst <marijn@monomelodies.nl>
2018-10-05 15:21:52 +0200

    we can handle this smarter

--- recipes/routing/Recipe.php
+++ recipes/routing/Recipe.php
@@ -2,12 +2,12 @@
 
 use Codger\Generate\Recipe;
 
-return function (int $api, ...$modules) : Recipe {
+return function (...$modules) : Recipe {
     $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
         protected $template = 'routing.html.twig';
     };
     $recipe->set('modules', $modules);
-    $recipe->set('api', $api);
+    $recipe->set('api', $this->askedFor('api'));
     $recipe->output('src/routing.php');
     return $recipe;
 };
