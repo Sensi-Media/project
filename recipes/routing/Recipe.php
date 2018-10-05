78e361a3873d67a945eedb7fa57b25b8d9a383cb
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-05 11:25:00 +0200

    add filters for normalization

--- recipes/routing/Recipe.php
+++ recipes/routing/Recipe.php
@@ -3,7 +3,14 @@
 use Codger\Generate\Recipe;
 
 return function (int $api, ...$modules) : Recipe {
-    $recipe = new class(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'))) extends Recipe {
+    $twig = new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'));
+    $twig->addFilter('normalize', new Twig_SimpleFilter(function (string $module) : string {
+        return strtolower(str_replace('\\', '-', $module));
+    }));
+    $twig->addFilter('fordb', new Twig_SimpleFilter(function (string $module) : string {
+        return strtolower(str_replace('-', '_', $module));
+    }));
+    $recipe = new class($twig) extends Recipe {
         protected $template = 'routing.html.twig';
     };
     $recipe->set('modules', $modules);
