002a4b1be38cdd605f1232e92d42768a30f2959d
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-05 11:29:25 +0200

    oopsie

--- recipes/routing/Recipe.php
+++ recipes/routing/Recipe.php
@@ -4,10 +4,10 @@ use Codger\Generate\Recipe;
 
 return function (int $api, ...$modules) : Recipe {
     $twig = new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates'));
-    $twig->addFilter('normalize', new Twig_SimpleFilter(function (string $module) : string {
+    $twig->addFilter(new Twig_SimpleFilter('normalize', function (string $module) : string {
         return strtolower(str_replace('\\', '-', $module));
     }));
-    $twig->addFilter('fordb', new Twig_SimpleFilter(function (string $module) : string {
+    $twig->addFilter(new Twig_SimpleFilter('fordb', function (string $module) : string {
         return strtolower(str_replace('-', '_', $module));
     }));
     $recipe = new class($twig) extends Recipe {
