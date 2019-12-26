c20390fff1d2a278e4d7413460339d3ee1c69dc3
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-26 15:45:09 +0100

    hold on, vendor is an option

--- recipes/Dependencies.php
+++ recipes/Dependencies.php
@@ -16,13 +16,15 @@ class Dependencies extends Recipe
     /** @var string */
     protected $_template = 'dependencies.html.twig';
 
-    public function __invoke(string $vendor, string $session) : void
+    public function __invoke(string $session) : void
     {
         $this->setTwigEnvironment(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__, 2).'/templates')));
         $this->output('src/dependencies.php');
-        switch ($this->vendor) {
-            case 'mysql': $this->set('vendor', 'Mysql'); break;
-            case 'pgsql': $this->set('vendor', 'Postgresql'); break;
+        if (isset($this->vendor)) {
+            switch ($this->vendor) {
+                case 'mysql': $this->set('vendor', 'Mysql'); break;
+                case 'pgsql': $this->set('vendor', 'Postgresql'); break;
+            }
         }
         array_walk($this->modules, function (&$repository) {
             $repository = [
