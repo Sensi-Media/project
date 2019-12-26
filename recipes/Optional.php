e23793802f6919ddf5044e4bbe103bf3225bc542
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-26 15:55:37 +0100

    convert to actual recipe

---
+++ recipes/Optional.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Codger\Sensi;
+
+use Codger\Generate\Recipe;
+use Codger\Generate\Language;
+
+class Optional extends Recipe
+{
+    /** @var array */
+    public $module = [];
+
+    /** @var string */
+    protected $_template = 'sass.html.twig';
+
+    public function __invoke() : void
+    {
+        $this->setTwigEnvironment(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/templates')));
+        array_walk($this->module, function (&$module) {
+            $module = Language::convert($module, Language::TYPE_PATH);
+        });
+        $this->set('modules', $this->module);
+        $this->output('src/optional.scss');
+    }
+}
+
