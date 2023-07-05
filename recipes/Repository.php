19b78efba956b8d270ea574d916933a815ce7749
Marijn Ophorst <marijn@sensimedia.nl>
2023-07-05 17:16:56 +0200

    this is more sensi-specific, so should go here

---
+++ recipes/Repository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Codger\Sensi;
+
+use Codger\Php\Klass;
+use Codger\Php\Method;
+use Codger\Generate\Language;
+
+/**
+ * Generate a Monolyth-style repository.
+ */
+class Repository extends Klass
+{
+    public string $table;
+
+    public function __invoke(string $name = null) : void
+    {
+        if (!isset($name)) {
+            $this->ask("What namespace shall we put this in?", function (string $answer) use (&$name) : void {
+                $name = $answer;
+            });
+        }
+        $name = Language::convert($name, Language::TYPE_PHP_NAMESPACE);
+        $var = Language::convert($name, Language::TYPE_VARIABLE);
+        while (!isset($this->table)) {
+            if (isset($name)) {
+                $this->table = strtolower(str_replace('\\', '_', $name));
+            }
+            $this->ask("Which table should we use? [{$this->table}]", function (string $answer) : void {
+                if (strlen($answer)) {
+                    $this->table = $answer;
+                }
+            });
+        }
+        $this->setNamespace($name)
+            ->setName('Repository')
+            ->usesNamespaces(
+                'Sensimedia\\Supportery\\DatabaseRepository',
+            )
+            ->extendsClass('DatabaseRepository')
+            ->addMethod('all', function () : array {}, function (Method $method) : string {
+                return <<<EOT
+return \$this->list(\$this->select());
+EOT;
+            })
+            ->addMethod('find', function (int $id) :? \Model {}, function (Method $method) : string {
+                return <<<EOT
+return \$this->findByIdentifier(\$id);
+EOT;
+            })
+            ->output(Language::convert($name, Language::TYPE_PATH).'/Repository.php')
+            ->info(<<<EOT
+Don't forget to register a dependency for this repository, e.g.
+
+\$container->register(function (&\${$var}Repository) {
+    \${$var}Repository = new $name\\Repository;
+});
+EOT
+        );
+    }
+}
+
